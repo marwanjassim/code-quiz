e9168bc228fd0053ec5e091dca3c9cd0dd201644
marwan jassim <marwanjassim87@gmail.com>
2019-12-07 17:55:12 -0800

    made the score page

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function timerFunction() {
     disp.innerHTML = "0"
     gameOver(0)
   } else {
-    disp.innerHTML = "your score is: " + time
+    disp.innerHTML = time
   }
 }
 
@@ -25,21 +25,64 @@ function startGame() {
 }
 
 function saveScore(score) {
+    var name = document.getElementById('name').value
+    var scores = window.localStorage.getItem('scores')
+
+    // If no array is saved, make an empty one first
+    if (scores === null) {
+        scores = []
+    } else {
+        scores = JSON.parse(scores)
+    }
+
+    // Save score
+    var savedScore = {
+        name: name,
+        score: score
+    }
+    scores.push(savedScore)
+
+    // Save to local storage
+    var scores = window.localStorage.setItem('scores', JSON.stringify(scores))
+    
+    // Show high scores
+    showScores()
 }
 
 function clearScores() {
-
+    window.localStorage.clear()
+    showScores()
 }
 
 function showScores() {
+    var scores = window.localStorage.getItem('scores')
 
+    // If no array is saved, make an empty one first
+    if (scores === null) {
+        scores = []
+    } else {
+        scores = JSON.parse(scores)
+    }
+
+    // scores = []
+    // score = {name:"Alex", score: 32}
+    var output = []
+    output.push("<ul>")
+    scores.forEach(function(score) {
+        output.push(`<li><b>${score.name}</b>: ${score.score}</li>`)
+    })
+    output.push("</ul>")
+    output.push("<button onclick='window.location.reload()'>Back</button>")
+    output.push("<button onclick='clearScores()'>Clear</button>")
+    quizArea.innerHTML = output.join("")
 }
 
 function gameOver(finalTime) {
   // Stop the game timer
   clearInterval(timer)
   var html = `
-  <h1>Quiz Over</h1>
+  <h1>Quiz over</h1>
+  <p>Your final score is ${finalTime}.</p>
   <input id="name" type="text" placeholder="Please type in your name"></input>
   <button onclick="saveScore(${finalTime})">Save</button>`
   quizArea.innerHTML = html
